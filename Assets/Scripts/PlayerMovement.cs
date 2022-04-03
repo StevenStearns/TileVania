fd5467ec53c47e5f881773acbc384fa4a39b3bea
steven stearns <stearn102@morrisville.edu>
2022-04-02 21:25:18 -0400

    Jumpy jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float fltRunSpeed = 10f;
+    [SerializeField] float fltJumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
 
@@ -27,6 +28,15 @@ public class PlayerMovement : MonoBehaviour
    {
        moveInput = value.Get<Vector2>();
        Debug.Log(moveInput);
+   }
+
+   void OnJump(InputValue value)
+   {
+       if(value.isPressed)
+       {
+           // do stuff
+           myRigidbody.velocity += new Vector2 (0f, fltJumpSpeed);
+       }
    }
 
    void Run()
