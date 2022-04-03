fe6f2c4226eaafb00b1e50f5b02551bedd09e8b1
steven stearns <stearn102@morrisville.edu>
2022-04-02 21:07:11 -0400

    Flip player sprite.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
    void OnMove(InputValue value)
@@ -31,5 +32,14 @@ public class PlayerMovement : MonoBehaviour
        myRigidbody.velocity = playerVelocity;
    }
 
+    void FlipSprite()
+    {
+        bool blPlayerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
 
-}
+        if (blPlayerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+        
+    }
+} // player movement
